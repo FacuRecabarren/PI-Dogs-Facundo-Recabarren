b19d599176ca482247078803ab02c3903c06840a
FacuRecabarren <facundorecabarren10@gmail.com>
2023-05-28 00:59:02 -0300

    Update

--- client/src/components/Paginated/Paginated.jsx
+++ client/src/components/Paginated/Paginated.jsx
@@ -1,28 +1,31 @@
 import React from "react";
-import style from "./paginated.module.css"
+import style from "./paginated.module.css";
 
-const Paginated = ({dogsPerPage, dogs, pagination}) => {
+const Paginated = ({ dogsPerPage, dogs, currentPage, pagination }) => {
+  const totalPages = Math.ceil(dogs / dogsPerPage);
+  const pages = [];
 
-    const pages = [];
+  for (let i = 0; i < totalPages; i++) {
+    pages.push(i + 1);
+  }
 
-    for (let i=0; i <= Math.floor(dogs/dogsPerPage); i++){
-        pages.push(i+1)
-    } // The number of pages is determined by dividing the total number of dogs by the number of dogs per page, rounding down with the Math.floor() function.
 
   return (
-    <nav>
-        <ul className={style.pagination}>
-            {
-            pages && pages.map(num => (
-                <li className={style.pageNumber} key={num}>
-
-                        <a className={style.link} onClick={() => pagination(num)}>{num}</a>    
-                    
-                </li>
-            ))}
-        </ul>
-    </nav>
+    <div>
+      <ul className={style.pagination}>
+        {pages.map((num) => (
+          <li className={style.pageNumber} key={num}>
+            <a
+              className={style.link}
+              onClick={() => pagination(num)}
+            >
+              {num}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default Paginated;
